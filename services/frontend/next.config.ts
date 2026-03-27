49f3224a0d192b2ec012aaf8aa7ceccdb0a90fd1
MrPink-96 <bugattiveyron696@gmail.com>
2026-03-27 12:00:00 +0300

    doc

--- services/frontend/next.config.ts
+++ services/frontend/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "localhost",
+        hostname: process.env.API_HOST || "localhost",
         port: "8080",
       },
     ],
